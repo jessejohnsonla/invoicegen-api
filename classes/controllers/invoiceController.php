81d9e1594147eee731305eb8425fdf615f25a84a
Jesse Johnson <jessejohnsonla@gmail.com>
2018-10-30 21:49:30 -0700

    commit

--- classes/controllers/invoiceController.php
+++ classes/controllers/invoiceController.php
@@ -12,41 +12,40 @@ class invoiceController extends AbstractController
                 $invoice_id = $request->url_elements[1];
                 return $this->readInvoice($invoice_id);
             break;
-            // case 4:
-            //     $invoice_id = $request->url_elements[1];
-            //     $contenttype = $request->url_elements[2]; //pdf
-            //     $emails = $request->url_elements[3];
-            //     return $this->generateInvoice($invoice_id, $contenttype, $emails);
-            // break;
         }
     }
     
     public function post($request) //create
     {
-        switch (count($request->url_elements)) {
-            case 1:
-                return null;
-            break;
-        }
+        $d = file_get_contents("php://input");
+        $json = json_decode($d);
+        updateInvoice($json);
     }
     
     public function put($request) //update
     {
-        switch (count($request->url_elements)) {
-            case 1:
-            break;
-        }
+        $d = file_get_contents("php://input");
+        $json = json_decode($d);
+        updateInvoice($json);
     }
+
     public function delete($request) {
 
     }
     
-    
+    public function createInvoice($invoice) {
+        $db = new mysqli(  "localhost:3306", "root", "!mysql!", "invoicegen" ); 
+        $res = $db->query( "CALL CreateInvoice($id)"); 
+        $res->exec();
+        return readInvoice($invoice.ID);
+    }
+    protected function updateInvoice($invoice) {
+
+    }
 
     protected function readInvoices()
     {
         $db = new mysqli(  "localhost:3306", "root", "!mysql!", "invoicegen" ); 
-
         $res = $db->query( "CALL GetInvoices()"); 
         $rows = array();
         while($row = $res->fetch_assoc()) {
@@ -58,7 +57,6 @@ class invoiceController extends AbstractController
     protected function readInvoice($id)
     {
         $db = new mysqli(  "localhost:3306", "root", "!mysql!", "invoicegen" ); 
-
         $res = $db->query( "CALL GetInvoice($id)"); 
         $invoices = array();
         while($row = $res->fetch_assoc()) {
@@ -66,11 +64,5 @@ class invoiceController extends AbstractController
         }
         $invoice = (count($invoices) > 0) ? $invoices[0] : null;
         return $invoice;
-    }
-
-    protected function writeInvoices($invoices)
-    {
-        //file_put_contents($this->articles_file, serialize($articles));
-        return true;
     }
 }
