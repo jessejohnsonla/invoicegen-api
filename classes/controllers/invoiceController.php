ab47c05ccd3ea8ff112c4b89b6b4ccaf6106bc2a
Jesse Johnson <jessejohnsonla@gmail.com>
2018-10-31 21:29:12 -0700

    updates

--- classes/controllers/invoiceController.php
+++ classes/controllers/invoiceController.php
@@ -2,6 +2,9 @@
 
 class invoiceController extends AbstractController
 {
+    protected function get_db_connection() {
+        return new mysqli("localhost:3306", "root", "!mysql!", "invoicegen"); 
+    }
     public function get($request)
     {
         switch (count($request->url_elements)) {
@@ -17,38 +20,92 @@ class invoiceController extends AbstractController
     
     public function post($request) //create
     {
-        $d = file_get_contents("php://input");
-        $json = json_decode($d);
-        updateInvoice($json);
+        return $this->handle_non_get_request($request, 'post');
     }
     
     public function put($request) //update
     {
-        $d = file_get_contents("php://input");
-        $json = json_decode($d);
-        updateInvoice($json);
+        return $this->handle_non_get_request($request, 'put');
     }
+
+    public function options($request) { //update due to cors
+        return $this->handle_non_get_request($request, 'options');
+    }
+
 
     public function delete($request) {
+        switch (count($request->url_elements)) {
+            case 2:
+                $invoice_id = $request->url_elements[1];
+                return $this->deleteInvoice($invoice_id);
+            break;
+        }
+    }
 
+    protected function handle_non_get_request($request, $request_method) {
+        $json = file_get_contents("php://input");
+        $invoice = json_decode($json);
+
+        if($request_method === 'post') {
+            return $this->createInvoice($invoice);
+        }
+        else{
+            return $this->updateInvoice($invoice);
+        }
     }
     
-    public function createInvoice($invoice) {
-        $db = new mysqli(  "localhost:3306", "root", "!mysql!", "invoicegen" ); 
-        $res = $db->query( "CALL CreateInvoice($id)"); 
-        $res->exec();
-        return readInvoice($invoice.ID);
+    protected function createInvoice($invoice) {
+        $result = 0;
+        $db = $this->get_db_connection();
+        $query = "CALL CreateInvoice( ".
+                    "'$invoice->BillToName',".
+                    "'$invoice->BillToAddress1',".
+                    "'$invoice->BillToAddress2',".
+                    "'$invoice->BillToCity',".
+                    "'$invoice->BillToState',".
+                    "'$invoice->BillToZipcode',".
+                    "'$invoice->ServiceDate',".
+                    "'$invoice->Terms',".
+                    "$invoice->TaxRate,".
+                    "$invoice->AmountPaid,".
+                    "'$invoice->DueDate',)";
+        $result = $db->query($query);  
+        
+        return $this->readInvoice($result);
     }
+    
     protected function updateInvoice($invoice) {
-
+        $result = 0;
+        $db = $this->get_db_connection();
+        $query = "CALL UpdateInvoice( ".
+                    "$invoice->ID,".
+                    "'$invoice->BillToName',".
+                    "'$invoice->BillToAddress1',".
+                    "'$invoice->BillToAddress2',".
+                    "'$invoice->BillToCity',".
+                    "'$invoice->BillToState',".
+                    "'$invoice->BillToZipcode',".
+                    "'$invoice->ServiceDate',".
+                    "'$invoice->Terms',".
+                    "$invoice->TaxRate,".
+                    "$invoice->AmountPaid,".
+                    "'$invoice->DueDate')";
+        $result = $db->query($query); 
+        return $result;
+    }
+    protected function deleteInvoice($invoice) {
+        $db = $this->get_db_connection();
+        $result = $db->query( "CALL DeleteInvoice($id)"); 
+        //$res->exec();
+        return $result;
     }
 
     protected function readInvoices()
     {
-        $db = new mysqli(  "localhost:3306", "root", "!mysql!", "invoicegen" ); 
-        $res = $db->query( "CALL GetInvoices()"); 
+        $db = $this->get_db_connection(); 
+        $result = $db->query( "CALL GetInvoices()"); 
         $rows = array();
-        while($row = $res->fetch_assoc()) {
+        while($row = $result->fetch_assoc()) {
             $invoices[] = $row;
         }
         return $invoices;
@@ -56,13 +113,14 @@ class invoiceController extends AbstractController
 
     protected function readInvoice($id)
     {
-        $db = new mysqli(  "localhost:3306", "root", "!mysql!", "invoicegen" ); 
-        $res = $db->query( "CALL GetInvoice($id)"); 
+        $db = $this->get_db_connection();
+        $result = $db->query( "CALL GetInvoice($id)"); 
         $invoices = array();
-        while($row = $res->fetch_assoc()) {
+        while($row = $result->fetch_assoc()) {
             $invoices[] = $row;
         }
         $invoice = (count($invoices) > 0) ? $invoices[0] : null;
         return $invoice;
     }
 }
+?>
--- classes/controllers/invoiceitemsController.php
+++ classes/controllers/invoiceitemsController.php
@@ -12,7 +12,37 @@ class invoiceitemsController extends AbstractController
         }
     }
     
+    public function post($request) //create
+    {
+        switch (count($request->url_elements)) {
+            case 2:
+                $invoice_id = $request->url_elements[1];
+                return $this->createInvoiceItem($invoice_id);
+            break;
+        }
+        $json = file_get_contents("php://input");
+        $item = json_decode($json);
+
+        if($request_method === 'post') {
+            return $this->createInvoiceItem($item);
+        }
+        else{
+            return $this->updateInvoiceItem($item);
+        }
+    }
     
+    protected function createInvoiceItem($item) {
+        $result = 0;
+        $db = $this->get_db_connection();
+        $query = "CALL CreateInvoiceItem( ".
+                    "'$item->InvoiceID',".
+                    "'$item->Description',".
+                    "'$item->Qty',".
+                    "'$item->Rate',)";
+        $result = $db->query($query);  
+        
+        return $result;
+    }
 
     protected function readInvoiceItems($invoiceid)
     {
